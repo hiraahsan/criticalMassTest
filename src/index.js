be8f19fca260f7e34f1ccdc59843e7c7de4677db
Hira <hiraahsan94@gmail.com>
2020-03-05 23:36:23 -0500

    categorizing and sorting menuitems

--- src/index.js
+++ src/index.js
@@ -4,3 +4,30 @@ import menu from "./menu";
 const menuItems = menu.items;
 
 console.log(menuItems);
+
+// - In `index.js`, loop over `menuItems` and categorize them into sections based on each items assigned `type`
+
+const pasta = [];
+const pizza = [];
+const starters = [];
+
+for (let i=0; i < menuItems.length; i++) {
+    if (menuItems[i].type === "pasta") {
+        pasta.push(menuItems[i]);
+    } else if (menuItems[i].type === "pizza") {
+        pizza.push(menuItems[i]);
+    } else if (menuItems[i].type === "starters") {
+        starters.push(menuItems[i]);
+    }
+}
+
+const sortOrder = (category) => {
+    category.sort((a, b) => {
+        return a.menuOrder - b.menuOrder;
+    })
+}
+
+sortOrder(pasta);
+sortOrder(pizza);
+sortOrder(starters);
+
